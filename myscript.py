e1f01b2b4a51a3e731e8f8caf7a9ac74e682e10f
YasmienHelmy <86154182+YasmienHelmy@users.noreply.github.com>
2021-08-09 00:30:32 +0200

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -6,3 +6,16 @@ print ("yasmeenkhedry2020@gmail.com")
 print ("@sam")
 print ("GenomicsTranscriptomicsvaccineinformatics")
 print ("@Jaz")
+
+#calculating hamming distance
+ 
+def h_d_loop(str_1,str_2) :
+     h_distance = 0
+     for position in range(len(str_1)):
+         if str_1[position] != str_2[position] :
+            h_distance += 1 
+     return h_distance
+ 
+str_1= "Jaz"
+str_2= "Sam"
+print (h_d_loop(str_1,str_2))
